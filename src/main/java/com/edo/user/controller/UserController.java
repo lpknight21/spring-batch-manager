93665a50291d4fc540e0580c5aebb9ec100fae33
louis.knight <louis.knight@edointeractive.com>
2016-02-09 10:05:30 -0600

    Changed user to users.

--- src/main/java/com/edo/user/controller/UserController.java
+++ src/main/java/com/edo/user/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("user")
+@RequestMapping("users")
 public class UserController {
 
     @Autowired
